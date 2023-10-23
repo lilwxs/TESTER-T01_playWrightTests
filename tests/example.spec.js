62cfcb2e5a620616dc026f254c1bd4a66cf82fc4
lilwxs <124787909+lilwxs@users.noreply.github.com>
2023-10-24 04:33:25 +0700

    UPLOAD 4:33 24/10/2023

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,8 +8,5 @@ test('test', async ({ page }) => {
   await page.getByTestId('royal_pass').click();
   await page.getByTestId('royal_pass').fill('LilWxs021124');
   await page.getByTestId('royal_login_button').click();
-  await page.getByRole('button', { name: 'Bellies ơi, bạn đang nghĩ gì thế?' }).click();
-  await page.getByRole('paragraph').click();
-  await page.getByLabel('Bellies ơi, bạn đang nghĩ gì thế?').fill('Hãy thường xuyên ngủ sớm nhé, chứ thức nhiều có hại sk!');
-  await page.getByLabel('Đăng', { exact: true }).click();
+  await expect(page).toHaveURL('https://www.facebook.com/');
 });
